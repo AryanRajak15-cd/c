fe560c0a01d872307d07542769aaecd7497c8aeb
Aryan Rajak <rajakaryan956@gmail.com>
2025-09-30 20:58:37 +0530

    Add files via upload

---
+++ lec1/lec1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>   
+
+int main() {
+   
+    int age = 19;         
+    float pi = 3.14;       
+    char grade = 'A';      
+
+    // Print karna
+    printf("Hello I am Aryan\n");     
+    printf("Age = %d\n", age);       
+    printf("Pi = %f\n", pi);        
+    printf("Grade = %c\n", grade);   
+    return 0;
+}
