bcd9223187143baf04cca9a528e35cdf0136997a
Aryan Rajak <rajakaryan956@gmail.com>
2025-09-30 21:20:31 +0530

    Add files via upload

---
+++ lec2/mathslogic.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+   
+int a=6;
+int b=3;
+
+//addition
+int sum=a+b;
+printf("%d \n",sum);
+
+
+//substration
+int sub=a-b;
+printf("%d \n",sub);
+
+//multiplication
+int mult=a*b;
+printf("%d \n",mult);
+
+//division//
+int div=a/b;
+printf("%d \n",div);
+}
---
+++ lec2/operator.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{   
+    
+     printf("%d \n", 4>9 || 2>3);        // OR operator 
+    
+
+     printf("%d \n", 10>9 && 6>3);       //AND "&&" operator
+    
+
+     printf("%d \n", !((10>9 ) && (6>4)));        //NOT operator //
+
+
+
+    }
