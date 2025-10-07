15d5d18c5ce39c916ec07f8a6255a3b75ed0dee8
Aryan Rajak <rajakaryan956@gmail.com>
2025-10-07 22:06:12 +0530

    continue command

---
+++ lec4/continue.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+
+    for (int i=0 ; i<=50 ; i++)
+    {
+        if (i % 2 == 1)
+        {
+
+          continue;
+        }
+        printf("%d \n",i);
+        
+    }
+    return 0;
+}
